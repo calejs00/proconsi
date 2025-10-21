0539825d4115e8676920fbb1f3ec190c6c676dde
Clara Alejos <calejs00@estudiantes.unileon.es>
2025-10-21 10:37:08 +0200

    arreglo casos

--- EJERCICIOS/operacionesNumeros.py
+++ EJERCICIOS/operacionesNumeros.py
@@ -1,3 +1,9 @@
+#CASOS DE ENTRADA QUE HE TENIDO EN CUENTA
+#-Que el usuario meta un valor no numérico o un valor nulo
+#-Que el usuario meta un número negativo o decimal de decimales
+#-Que el usuario meta un 0 en la división o módulo
+
+
 #Primero defino las funciones de lo que se me pide hacer, todas redondeando a los numeros decimales requeridos
 #función suma
 def sum(n1,n2, dec):
@@ -9,6 +15,8 @@ def rest(n1,n2, dec):
 
 #función división
 def div(n1,n2, dec):
+    if n2 == 0:
+        return "Error: División por cero"
     return round(n1/n2, dec)
 
 #función multiplicación
@@ -17,6 +25,8 @@ def mult(n1,n2, dec):
 
 #función módulo
 def mod(n1,n2, dec):
+    if n2 == 0:
+        return "Error: Módulo por cero"
     return round(n1%n2, dec)
 
 #función para saber si el primer numero es mayor, igual o menor
@@ -30,16 +40,46 @@ def mayor(n1,n2):
 
 def main():
     #pido los numeros por pantalla
-    num1 = float(input("Introduce la primera cantidad: "))
-    num2 = float(input("Introduce la segunda cantidad: "))
-    numDecimales = int(input("Di cuántos decimales quieres redondear:"))
+    numDecimales = "c"
+    num1 = "a"
+    num2 = "b"
+    while (isinstance(numDecimales, str) or (int(numDecimales) and numDecimales<0)) or isinstance(num1, str) or isinstance(num2, str):
+        num1 = input("Introduce la primera cantidad: ")
+        if isinstance(num1, str):
+            try:
+                num1 = float(num1)
+            except ValueError:
+                print("Por favor, introduce un número válido. Volvamos a empezar")
+                continue
+
+        num2 = input("Introduce la segunda cantidad: ")
+        if isinstance(num2, str):
+            try:
+                num2 = float(num2)
+            except ValueError:
+                print("Por favor, introduce un número válido. Volvamos a empezar")
+                continue
+
+        numDecimales = input("Di cuántos decimales quieres redondear: ")
+        if isinstance(numDecimales, str):
+            try:
+                numDecimales = int(numDecimales)
+            except ValueError:
+                print("Por favor, introduce un número válido para los decimales. Volvamos a empezar")
+                continue 
+
+        if numDecimales < 0:
+            print("El número de decimales debe ser un entero no negativo. Volvamos a empezar")
+        else:
+            break
+
 
     #printeo y ejecuto todo lo que se pide
-    print("La suma de",num1, "y", num2, "es", sum(num1, num2, numDecimales))
-    print("La resta de",num1, "y", num2, "es", rest(num1, num2, numDecimales))
-    print("La división de",num1, "y", num2, "es", div(num1, num2, numDecimales))
-    print("La multiplicación de",num1, "y", num2, "es", mult(num1, num2, numDecimales))
-    print("El módulo de",num1, "y", num2, "es", mod(num1, num2, numDecimales))
+    print("La suma de",num1, "y", num2, "es", format(sum(num1, num2, numDecimales),f".{numDecimales}f"))
+    print("La resta de",num1, "y", num2, "es", format(rest(num1, num2, numDecimales),f".{numDecimales}f"))
+    print("La división de",num1, "y", num2, "es", format(div(num1, num2, numDecimales),f".{numDecimales}f"))
+    print("La multiplicación de",num1, "y", num2, "es", format(mult(num1, num2, numDecimales),f".{numDecimales}f"))
+    print("El módulo de",num1, "y", num2, "es", format(mod(num1, num2, numDecimales),f".{numDecimales}f"))
     mayor(num1, num2)
     return
 
